5015b5e0edb1b397a75bb40b1abaacf39ae0b73f
Ethan Smith <ethan@onebytegone.com>
2015-03-17 19:27:22 -0400

    Added start of router

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 var express = require('express');
 
 var app = express();
+var ai = require('./src/routes/ai');
+
+app.get('/', function (req, res) {
+     res.send('Hello World!');
+});
+
+app.use('/ai', ai);
 
 app.listen(3000);
 console.log('listening on port 3000');
