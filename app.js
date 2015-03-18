c4ddc983ba04969a72b9b17ebe13c666e51e54fb
Ethan Smith <ethan@onebytegone.com>
2015-03-17 20:17:25 -0400

    Added redis/jwt sessions. Added config

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 
-var express = require('express');
+var express = require('express'),
+    JWTRedisSession = require("jwt-redis-session"),
+    redis = require("redis"),
+    config = require("config");
 
-var app = express();
-var ai = require('./src/routes/ai');
+var redisClient = redis.createClient(),
+    app = express();
+
+app.use(JWTRedisSession({
+   client: redisClient,
+   secret: config.get('jwt.secret'),
+   keyspace: "sess:",
+   maxAge: 86400,
+   algorithm: "HS256",
+   requestKey: "jwtSession",
+   requestArg: "jwtToken"
+}));
 
 app.get('/', function (req, res) {
-     res.send('Hello World!');
+   res.send('Hello World!');
 });
 
+var ai = require('./src/routes/ai');
 app.use('/ai', ai);
 
 app.listen(3000);
--- src/routes/ai.js
+++ src/routes/ai.js
@@ -1,4 +1,6 @@
-var express = require('express');
+var express = require('express'),
+    config = require("config");
+
 var router = express.Router();
 
 // middleware specific to this router
@@ -16,5 +18,22 @@ router.get('/', function(req, res) {
 router.get('/about', function(req, res) {
      res.send('About birds');
 })
+
+// define a test JWT route
+router.get('/jwt', function(req, res) {
+   // this will be stored in redis
+   req.jwtSession.user = { 'color': 'blue' };
+
+   // this will be attached to the JWT
+   var claims = {
+      iss: config.get('jwt.issuer'),
+      aud: config.get('jwt.audience')
+   };
+
+   // Create JWT and add to reponse
+   req.jwtSession.create(claims, function(error, token){
+      res.json({ token: token });
+   });
+});
 
 module.exports = router;
