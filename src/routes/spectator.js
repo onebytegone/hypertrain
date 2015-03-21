8de85a11cc63f0cbfdac2bdc193bf63840c9ff67
Ethan Smith <ethan@onebytegone.com>
2015-03-21 18:54:59 -0400

    Added game info lookup

--- src/routes/spectator.js
+++ src/routes/spectator.js
@@ -1,16 +1,51 @@
 var express = require('express'),
     jwt = require("jwt-simple"),
     config = require("config"),
-    game = require("../model/game");
+    game = require("../model/game"),
+    debug = require('debug')('spectator');
 
 var router = express.Router();
 
-
-// define the home page route
+// define the base page route
 router.get('/', function(req, res) {
    game.allGames(function (games) {
       res.jsonp(games);
    });
 })
+
+// define the game page route
+router.get('/:gameident', function(req, res) {
+   game.fetchGame(req.gameident, function(gameModel) {
+      res.jsonp({
+         'meta': {
+            'code': 200
+         },
+         'payload': {
+            'game': gameModel
+         }
+      });
+   });
+})
+
+// route middleware to validate :gameident
+router.param('gameident', function(req, res, next, gameident) {
+   debug('validating gameident: ['+gameident+']');
+
+   if (gameident.match(/[[:xdigit:]]*/g)) {
+      req.gameident = gameident;
+      next();
+   }else {
+      debug('gameident ['+gameident+'] is not valid');
+
+      res.jsonp({
+         'meta': {
+            'error': 'invalid game identifier',
+            'code': 400
+         },
+         'payload': {}
+      });
+   }
+});
+
 
 module.exports = router;
